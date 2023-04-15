87c39dad756569ca2aca6311d82e383399667a7b
Dmitry Teleshev <teleshev120@mail.ru>
2023-04-15 19:14:10 +0300

    test ErrorBoundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,15 @@
-import { Suspense } from 'react'
-
+import { Suspense, useEffect } from 'react'
 import './styles/index.css'
 import { AppRouter } from './providers/RouterProvider'
+import { MainPage } from 'pages/MainPage'
 
 const App = () => {
+    useEffect(() => {
+        if (Math.random() < 0.5) {
+            throw new Error('error')
+        }
+    }, [])
+
     return (
         <div>
             <Suspense fallback="">
