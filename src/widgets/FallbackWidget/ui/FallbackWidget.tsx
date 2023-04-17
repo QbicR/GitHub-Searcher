7cb820ed126b5ee4f5f71d9c2b401b9a1a2c0966
Dmitry Teleshev <teleshev120@mail.ru>
2023-04-17 18:40:15 +0300

    fix fallback widget to content center

--- src/widgets/FallbackWidget/ui/FallbackWidget.tsx
+++ src/widgets/FallbackWidget/ui/FallbackWidget.tsx
@@ -2,7 +2,7 @@ import { Loader } from 'shared/ui/Loader/Loader'
 
 export const FallbackWidget = () => {
     return (
-        <div className="w-full h-screen bg-slate-800">
+        <div className="flex items-center justify-center w-full h-screen bg-slate-800">
             <div className="flex items-center justify-center flex-col w-full max-w-6xl h-4/5 p-8 gap-8  border rounded-lg shadow bg-gray-800 border-gray-700">
                 <Loader />
             </div>
