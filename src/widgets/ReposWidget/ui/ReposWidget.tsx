0694727933974bc7aee3e31a58aae92dd206300a
Dmitry Teleshev <teleshev120@mail.ru>
2023-04-19 15:16:32 +0300

    fix repos widget UI bug

--- src/widgets/ReposWidget/ui/ReposWidget.tsx
+++ src/widgets/ReposWidget/ui/ReposWidget.tsx
@@ -9,22 +9,22 @@ import { Loader } from 'shared/ui/Loader/Loader'
 export const ReposWidget = memo(() => {
     const userData = useSelector(getUserState)
     const reposData = useSelector(getReposState)
-    const { login, avatar_url: avatar, error } = userData
-    const { repositories, loading } = reposData
+    const { login, avatar_url: avatar, error: userError, loading: userLoading } = userData
+    const { repositories } = reposData
 
-    if (loading) {
+    if (userLoading) {
         return (
             <div className="flex items-center justify-center flex-col w-full max-w-6xl h-4/5 p-8 gap-8  border rounded-lg shadow bg-gray-800 border-gray-700">
                 <Loader />
             </div>
         )
-    } else if (error) {
+    } else if (userError) {
         return (
             <div className="flex items-center justify-center flex-col w-full max-w-6xl h-4/5 p-8 gap-8 border rounded-lg shadow bg-gray-800 border-gray-700">
-                <div className="text-3xl text-center font-medium text-gray-200">{error}</div>
+                <div className="text-3xl text-center font-medium text-gray-200">{userError}</div>
             </div>
         )
-    } else if (repositories && !error) {
+    } else if (repositories) {
         return (
             <div className="flex items-center justify-start flex-col w-full max-w-6xl h-4/5 p-8 gap-8  border rounded-lg shadow bg-gray-800 border-gray-700">
                 <UserInfo login={login} avatar={avatar} />
