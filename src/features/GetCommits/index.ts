d0e32e8e1a7dd7fb5d851e1957610072a3627672
Dmitry Teleshev <teleshev120@mail.ru>
2023-05-06 15:54:28 +0300

    change commites selector

--- src/features/GetCommits/index.ts
+++ src/features/GetCommits/index.ts
@@ -1,4 +1,8 @@
-export { getCommitesState } from './model/selectors/getCommitesState'
+export {
+  getCommitesStateData,
+  getCommitesStateError,
+  getCommitesStateLoading,
+} from './model/selectors/getCommitesState'
 export { getCommitesData } from './model/services/getCommitesData'
 export { commitesAction, commitesReducer } from './model/slice/commitesSlice'
 export type { CommitesType, CommitType } from './model/types/commitType'
